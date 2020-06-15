2ea65f9706584e36e8a665bd41cab1b9a75792c1
Florent Bednarek <florent.bednarek@eisti.fr>
2020-06-15 18:42:42 +0200

    messagerie en cours

--- src/messagerie/listeMatch.js
+++ src/messagerie/listeMatch.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import Cookies from 'js-cookie';
 
 import {URL_API} from '../App';
+import ListMessages from './message'
+
+
 class ListMatch extends Component {
 	constructor(props) {
         super(props);
         this.state = {
-        		list : ""
+        		list : "",
+        		show : 0,
+        		id : 0,
+        		id2: 0
         }
     }
 
@@ -34,13 +40,21 @@ class ListMatch extends Component {
 
 	}
 
+	handleClick(id,id2){
+		this.setState({show : 1});
+		this.setState({id : id});
+		this.setState({id2 : id2})
+	}
 
 	affMatch() {		
 		let ret = "";
 		let match = this.state.list;
 		match = match.slice(0,-1);
 		let lmatch = match.split(';');
-		ret = lmatch.map(el => <tr style={{border:"solid black 2px",borderRadius : '10px'}}><td>{el.split('-')[0]}</td><td>{el.split('-')[1]}</td></tr>);
+		ret = lmatch.map(el => <tr style={{border:"solid black 2px",borderRadius : '10px'}} onClick={ () => {this.handleClick(el.split('-')[0],el.split('-')[1])}}>
+									<td>{el.split('-')[0]}</td>
+									<td>{el.split('-')[1]}</td>
+								</tr>);
 		return (ret)
 	}
 
@@ -49,14 +63,22 @@ class ListMatch extends Component {
 	}
 	render(){
 		return(
-			<div className="listeMatch" >
+			
+			<div>
+				{this.state.show == 1 &&
+					<div className="messages">
+						<ListMessages id={this.state.id} id2={this.state.id2} />
+					</div> 
+				}
+				<div className="listeMatch" >						
+					<table>
+						<tbody> 
+							{this.affMatch()}	
+						</tbody>
+					</table>
+				</div>
 				
-				
-				<table>
-					<tbody>
-						{this.affMatch()}	
-					</tbody>
-				</table>
+
 			</div>
 		)
 	}
